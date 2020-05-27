f0faae4c40ae59e8ac2cb6263dedec2209f186c8
biglogo <5697832+biglogo@user.noreply.gitee.com>
2020-05-27 14:48:17 +0800

    enter

--- src/main.js
+++ src/main.js
@@ -2,18 +2,60 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+<<<<<<< HEAD
+=======
+import 'default-passive-events'
+>>>>>>> report
 // 导入全局样式表
 import './assets/css/global.css'
 
 // 导入全局样式表
 import './assets/fonts/iconfont.css'
 
+<<<<<<< HEAD
 import axios from 'axios'
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+=======
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+// 导入富文本编辑器对应的样式
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+import TreeTable from 'vue-table-with-tree-grid'
+
+import axios from 'axios'
+// 配置请求的根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 请求拦截
+axios.interceptors.request.use(config => {
+  // 在发送请求之前做些什么
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+>>>>>>> report
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
+
+Vue.component('tree-table', TreeTable)
+
+// 将富文本编辑器注册为全局可用组件
+Vue.use(VueQuillEditor)
+Vue.filter('dateFormat', function(originVal) {
+  const dt = new Date(originVal)
+
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 
 new Vue({
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Login from '../components/Login.vue'
-import Home from '../components/Home.vue'
+import Login from '../components/Login'
+import Home from '../components/Home'
+import Welcome from '../components/welcome'
+import Users from '../components/users/Users'
+import Rights from '../components/power/Rights'
+import Roles from '../components/power/Roles'
+import Cate from '../components/goods/Cate'
+import Params from '../components/goods/Params'
+import GoodsList from '../components/goods/List'
+import Add from '../components/goods/Add'
+import Order from '../components/order/order'
+import Report from '../components/report/Report'
 
 Vue.use(VueRouter)
 
@@ -9,7 +19,23 @@ const router = new VueRouter({
   routes: [
     { path: '/', redirect: '/login' },
     { path: '/login', component: Login },
-    { path: '/home', component: Home }
+    {
+      path: '/home',
+      component: Home,
+      redirect: '/welcome',
+      children: [
+        { path: '/welcome', component: Welcome },
+        { path: '/users', component: Users },
+        { path: '/rights', component: Rights },
+        { path: '/roles', component: Roles },
+        { path: '/categories', component: Cate },
+        { path: '/params', component: Params },
+        { path: '/goods', component: GoodsList },
+        { path: '/goods/add', component: Add },
+        { path: '/orders', component: Order },
+        { path: '/reports', component: Report }
+      ]
+    }
   ]
 })
 
